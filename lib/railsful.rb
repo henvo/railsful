a8b1192c33bc5c53045a4ed5e06b0c9c7636f472
Henning Vogt <git@henvo.de>
2020-03-01 08:43:48 +0100

    Remove automatic module inclusion

--- lib/railsful.rb
+++ lib/railsful.rb
@@ -2,7 +2,6 @@ require 'active_support'
 
 require 'railsful/version'
 require 'railsful/exceptions'
-require 'railsful/railtie' if defined?(Rails)
 require 'railsful/serializable'
 require 'railsful/serializer'
 require 'railsful/deserializable'
